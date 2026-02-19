9b99f9e2ccc0d1997b330f0c1a9750c472aafabd
Juan Sebastian Cadena <juan.cadena02@uceva.edu.co>
2026-02-18 20:55:29 -0500

    Add CORS middleware configuration to FastAPI app

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.categories_router import category_router
 from routers.services_router import service_router
 from routers.products_router import product_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # agrega tus dominios
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],  # o lista explícita incluyendo ngrok-skip-browser-warning
+)
+
 app.include_router(service_router)
 app.include_router(category_router)
 app.include_router(product_router)
